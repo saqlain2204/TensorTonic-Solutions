0a94057ab123a0727e626952cc0412daf59a4f10
Mohammed Saqlain <saqlain2204@gmail.com>
2026-07-03 18:23:00 +0530

    Solve wasserstein-critic-loss

---
+++ Problems/wasserstein-critic-loss/wasserstein-critic-loss.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def wasserstein_critic_loss(real_scores, fake_scores):
+    """
+    Compute Wasserstein Critic Loss for WGAN.
+    """
+    # Write code here
+    return np.mean(fake_scores) - np.mean(real_scores)
