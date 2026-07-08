2172004e861230c68207ac112ac531df3206c9e0
Mohammed Saqlain <saqlain2204@gmail.com>
2026-07-08 22:38:39 +0530

    Solve percentiles

---
+++ Problems/percentiles/percentiles.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def percentiles(x, q):
+    """
+    Compute percentiles using linear interpolation.
+    """
+    # Write code here
+    x = np.array(x, dtype=float)
+    q = np.array(q, dtype=float)
+
+    return np.percentile(x, q, method='linear')
