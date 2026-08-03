68281906962b5ef408cf6ffbbeabb69da377560f
Mohammed Saqlain <saqlain2204@gmail.com>
2026-08-03 22:41:55 +0530

    Solve jaccard-similarity

---
+++ Problems/jaccard-similarity/jaccard-similarity.py
@@ -0,0 +1,8 @@
+def jaccard_similarity(set_a, set_b):
+    """
+    Compute the Jaccard similarity between two item sets.
+    """
+    # Write code here
+    set_a = set(set_a)
+    set_b = set(set_b)
+    return len(set_a & set_b) / len(set_a | set_b)
