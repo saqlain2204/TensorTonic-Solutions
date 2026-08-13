dd39b6a3af9c9fd8be04ab1cc7874399ece2c9bb
Mohammed Saqlain <saqlain2204@gmail.com>
2026-08-13 19:19:39 +0530

    Solve top-k-recommendations

---
+++ Problems/top-k-recommendations/top-k-recommendations.py
@@ -0,0 +1,10 @@
+def top_k_recommendations(scores, rated_indices, k):
+    """
+    Return indices of top-k unrated items by predicted score.
+    """
+    # Write code here
+    rated_indices = set(rated_indices)
+    unrated = [i for i in range(len(scores)) if i not in rated_indices]
+    unrated.sort(key=lambda i: scores[i], reverse=True)
+
+    return unrated[:k]
