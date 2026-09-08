f04edfc65e68aba1c6fb2e893a9506528ada646f
Mohammed Saqlain <saqlain2204@gmail.com>
2026-09-08 21:22:03 +0530

    [Correction] Solve jaccard-similarity

--- Problems/jaccard-similarity/jaccard-similarity.py
+++ Problems/jaccard-similarity/jaccard-similarity.py
@@ -1,8 +1,12 @@
-def jaccard_similarity(set_a, set_b):
+def jaccard_similarity(set_a: list, set_b: list) -> float:
     """
-    Compute the Jaccard similarity between two item sets.
+    Returns the Jaccard similarity of the two item collections.
     """
     # Write code here
     set_a = set(set_a)
     set_b = set(set_b)
-    return len(set_a & set_b) / len(set_a | set_b)
+
+    try:
+        return len(set_a & set_b) / len(set_a | set_b)
+    except ZeroDivisionError:
+        return 0.0
